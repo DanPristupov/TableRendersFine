47f0dfe7a8065962c78d70800dd635a697a6e4d5
Dan Pristupov <danil@danil.cz>
2017-01-24 08:03:56 +0100

    Add simpler example based on NSTextFIeld

---
+++ CustomTextFieldCell/CustomTextFieldCell/CustomTextField.swift
@@ -0,0 +1,15 @@
+//
+//  CustomTextField.swift
+//  CustomTextFieldCell
+//
+//  Created by Danil on 24/01/2017.
+//  Copyright © 2017 Eugene Mankovski. All rights reserved.
+//
+
+import Cocoa
+
+class CustomTextField: NSTextField {
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+    }
+}
